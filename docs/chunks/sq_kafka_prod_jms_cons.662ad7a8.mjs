3e9270aee53338fd70e4a31b69f2450f3c9e24a4
Niamh Thornbury <nthornbury@confluent.io>
2024-07-30 18:04:37 +0100

    removing files

--- docs/chunks/sq_kafka_prod_jms_cons.662ad7a8.mjs
+++
@@ -1,9 +0,0 @@
-async function getMod() {
-						return import('./sq_kafka_prod_jms_cons.07787177.mjs');
-					}
-					const collectedLinks = "@@ASTRO-LINKS@@";
-					const collectedStyles = "@@ASTRO-STYLES@@";
-					const collectedScripts = "@@ASTRO-SCRIPTS@@";
-					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts };
-
-export { defaultMod as default };
--- docs/chunks/version.cab770ef.mjs
+++
@@ -1,9 +0,0 @@
-async function getMod() {
-						return import('./version.51814c67.mjs');
-					}
-					const collectedLinks = "@@ASTRO-LINKS@@";
-					const collectedStyles = "@@ASTRO-STYLES@@";
-					const collectedScripts = "@@ASTRO-SCRIPTS@@";
-					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts };
-
-export { defaultMod as default };
