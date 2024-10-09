062d3734dc66cdc07f30e35f0e817cce69f5e9c4
Roman Kolesnev <rkolesnev@confluent.io>
2024-10-09 07:20:21 -0500

    read partner id from manifest with fallback to properties file (#56)

--- jms-bridge-server/src/main/java/io/confluent/amq/config/BridgeConfig.java
+++ jms-bridge-server/src/main/java/io/confluent/amq/config/BridgeConfig.java
@@ -8,11 +8,16 @@ import com.typesafe.config.Config;
 import io.confluent.csid.common.utils.accelerator.Accelerator;
 import io.confluent.csid.common.utils.accelerator.ClientId;
 import io.confluent.csid.common.utils.accelerator.Owner;
+import lombok.extern.slf4j.Slf4j;
 import org.inferred.freebuilder.FreeBuilder;
 
+import java.io.IOException;
+import java.net.URL;
 import java.time.Duration;
+import java.util.Enumeration;
 import java.util.Map;
 import java.util.Optional;
+import java.util.jar.Manifest;
 
 import static io.confluent.amq.config.BridgeConfigFactory.flattenConfig;
 import static io.confluent.amq.config.BridgeConfigFactory.getBridgeVersion;
@@ -40,7 +45,9 @@ public interface BridgeConfig {
 
   Optional<SecurityConfig> security();
 
+  @Slf4j
   class Builder extends BridgeConfig_Builder {
+    static String MANIFEST_PARTNER_ID_PROPERTY_NAME = "Partner-Id";
 
     public Builder() {
 
@@ -49,11 +56,11 @@ public interface BridgeConfig {
     public Builder(Config rootConfig) {
       Config bridgeConfig = rootConfig.getConfig("bridge");
       this.id(bridgeConfig.getString("id"))
-          .journals(new JournalsConfig.Builder(bridgeConfig.getConfig("journals")))
-          .putAllKafka(flattenConfig(bridgeConfig.getConfig("kafka")))
-          .putAllStreams(flattenConfig(
-              bridgeConfig.getConfig("streams")
-                  .withFallback(bridgeConfig.getConfig("kafka"))));
+              .journals(new JournalsConfig.Builder(bridgeConfig.getConfig("journals")))
+              .putAllKafka(flattenConfig(bridgeConfig.getConfig("kafka")))
+              .putAllStreams(flattenConfig(
+                      bridgeConfig.getConfig("streams")
+                              .withFallback(bridgeConfig.getConfig("kafka"))));
 
       if (bridgeConfig.hasPath("ha")) {
         Config haConfig = bridgeConfig.getConfig("ha");
@@ -62,26 +69,63 @@ public interface BridgeConfig {
 
       if (bridgeConfig.hasPath("routing")) {
         this.routing(new RoutingConfig.Builder(
-            bridgeConfig.getConfig("kafka"),
-            bridgeConfig.getConfig("routing")).build());
+                bridgeConfig.getConfig("kafka"),
+                bridgeConfig.getConfig("routing")).build());
       }
 
       if (bridgeConfig.hasPath("security")) {
         this.security(new SecurityConfig.Builder(bridgeConfig.getConfig("security")).build());
       }
 
-      if (bridgeConfig.hasPath(("partner_sfdc_id"))) {
-        this.partnerSFDCId(bridgeConfig.getString("partner_sfdc_id"));
-      }
+      this.partnerSFDCId(readPartnerSfdcId(bridgeConfig));
+
       this.clientId(new BridgeClientId.Builder()
-                      .partnerSFDCId(this.partnerSFDCId())
-                      .bridgeId(this.id())
-                      .build());
-      if(bridgeConfig.hasPath("kafka.ha.disabled")) {
+              .partnerSFDCId(this.partnerSFDCId())
+              .bridgeId(this.id())
+              .build());
+      if (bridgeConfig.hasPath("kafka.ha.disabled")) {
         this.kafkaHaDisabled(bridgeConfig.getBoolean("kafka.ha.disabled"));
       }
     }
 
+    /**
+     * Reads Partner Id from Manifest of the jar primarily and from Bridge configuration as fallback if not present in
+     * the manifest.
+     * @param bridgeConfig
+     * @return Optional of partner id
+     */
+    private Optional<String> readPartnerSfdcId(Config bridgeConfig) {
+      String partnerSFDCId = null;
+      log.debug("Reading partner id from manifest / configuration");
+      try {
+        Enumeration<URL> resources = getClass().getClassLoader().getResources("META-INF/MANIFEST.MF");
+        while (resources.hasMoreElements()) {
+          partnerSFDCId =
+                  new Manifest(resources.nextElement().openStream())
+                          .getMainAttributes()
+                          .getValue(MANIFEST_PARTNER_ID_PROPERTY_NAME);
+          if (partnerSFDCId != null) {
+            break;
+          }
+        }
+      } catch (IOException e) {
+        log.warn("Unable to find manifest from classPath Url for partner id injection: {}", e.getMessage());
+      }
+      if (partnerSFDCId == null) {
+        // If the manifest file does not contain the partnerId property, log the error and continue
+        // with the default value
+        log.debug("Manifest file does not contain the {} property. Will check properties next", MANIFEST_PARTNER_ID_PROPERTY_NAME);
+        if (bridgeConfig.hasPath(("partner_sfdc_id"))) {
+          partnerSFDCId = bridgeConfig.getString("partner_sfdc_id");
+        }
+      }
+      if (partnerSFDCId == null) {
+        log.debug("Partner Id not found in manifest or properties.");
+        return Optional.empty();
+      }
+      log.debug("Using Partner Id: {}", partnerSFDCId);
+      return Optional.of(partnerSFDCId);
+    }
   }
 
   @FreeBuilder
